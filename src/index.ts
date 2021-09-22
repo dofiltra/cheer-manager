3b3640b7d52708a06f72302f7fad95ef3762d911
memanot <menemty@gmail.com>
2021-09-22 14:03:25 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import cheerio, { CheerioAPI } from 'cheerio'
+
+type TCheerSettings = {
+  html: string
+}
+
+class CheerManager {
+  public $: CheerioAPI
+
+  constructor(s: TCheerSettings) {
+    this.$ = cheerio.load(s.html)
+  }
+
+  getHtml() {
+    return this.$.html()
+  }
+
+  rmComments() {
+    this.$.root()
+      .contents()
+      .filter(function () {
+        return this.type === 'comment'
+      })
+      .remove()
+  }
+
+  rmBySelector(selector: string, count: number = Number.MAX_SAFE_INTEGER) {
+    if (!selector) {
+      return
+    }
+    this.$(selector)
+      .slice(0, count)
+      .map((i:number, el:any) => this.$(el).remove())
+  }
+
+  rmByInnerText(
+    selector: string,
+    innerText?: string,
+    innerCount = Number.MAX_SAFE_INTEGER
+  ) {
+    if (!innerText || !selector) {
+      return
+    }
+
+    try {
+      this.$(selector)
+        .filter((i:number, el: any) =>
+          this.$(el.children[0]).text().includes(innerText!)
+        )
+        .slice(0, innerCount)
+        .map((i:number, el:any) => this.$(el).remove())
+    } catch (error: any) {
+      return { error}
+    }
+  }
+}
+
+export { CheerManager }
